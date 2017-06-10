09ff30749211d139944a208944fb59bbc575ccdd
DavidLee999 <MoonLee003@outlook.com>
2017-06-10 20:16:56 +0200

    default constructor

--- quick_test.cpp
+++ quick_test.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
-#include <vector>
+#include <string>
+//#include <vector>
 using namespace std;
+
+struct Work{
+	string author;
+	string name;
+	int year;
+};
 
 int main()
 {
-	vector<int> a {1,2,3,4,5};
-	cout<<a;
+	Work s9 { "Mike", "None", 1999 };
+	
+	Work s8 = s9;
+	
+	Work s7 {};
+	
+	cout << s9.author << " " << s9.name << " " << s9.year << endl;
+	cout << s8.author << " " << s8.name << " " << s8.year << endl;
+	cout << s7.author << " " << s7.name << " " << s7.year << endl;
 	
 	return 0;
 }
