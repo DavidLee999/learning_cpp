e3768671968ac32d59c4ba578c44943384c8e989
DavidLee999 <MoonLee003@outlook.com>
2017-06-17 22:00:24 +0200

    operator dereferencing

--- operator_dereferencing.cpp
+++ operator_dereferencing.cpp
@@ -17,7 +17,12 @@ struct Ptr1 {
 struct Ptr2 {
 	Ptr1* ptr;
 	
-	Ptr1& operator -> () const
+	/* Ptr1* operator -> () const // return a pointer 
+	{
+		return ptr;
+	} */
+	
+	Ptr1& operator -> () const // return the dereferencing, the address which holds the pointer *orig
 	{
 		return *ptr;
 	}
@@ -32,13 +37,17 @@ int main()
 	p1.orig = &o;
 	
 	cout << "Ptr1: p1->value = " << p1->value << endl; // object to be "pointed" should have the same name of its definition
-	                                                    // and cannot be "asigned" to an arbitrary name.
+	                                                    // and cannot be "asigned" to an arbitrary name or the name of the pointer.
 														
 	Ptr2 p2;
 	p2.ptr = &p1;
 	
-	cout << "Ptr2: p2->value = " << p2->value << endl; // pointed to the "final" object
+	cout << "Ptr2: p2->orig = " << p2->orig << endl; // pointed to the "final" object
 	
-	 
+	cout << "p1.operator->() = " << p1.operator->() << endl; // return a pointer, same as above
+	
+	cout << "p1.operator->()->value = " << p1.operator->()->value << endl;
+	
+	cout << "Ptr2: p2->orig->value = " << p2->orig->value << endl; // return the final value
 	return 0;
 }
