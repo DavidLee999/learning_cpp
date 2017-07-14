143ac9e422b86bd3bd8b8f143099717c40d3ca40
DavidLee999 <MoonLee003@outlook.com>
2017-07-14 21:01:20 +0200

    test

--- quick_test.cpp
+++ quick_test.cpp
@@ -26,10 +26,12 @@ constexpr Complex<double> operator ""i ( long double d )
 int main()
 {
 	map<int, char> s { {4,'a'}, {2,'k'}, {0,'2'}, {8,'o'} };
+    s[5] = 'l';
     for( map<int, char>::const_iterator a = s.cbegin(); a != s.cend(); ++a )
         std::cout << (*a).first << " " << (*a).second << '\t';
 
     unordered_map<int, char> s2 { {4,'a'}, {2,'k'}, {0,'2'}, {8,'o'} };
+    s2[0] = 't';
     std::cout << std::endl;
     for( unordered_map<int, char>::const_iterator a = s2.cbegin(); a != s2.cend(); ++a )
         std::cout << (*a).first << " " << (*a).second << '\t';
