67deaa0c63cb649a2aa818e315ab15f61185c08d
DavidLee999 <MoonLee003@outlook.com>
2017-07-29 20:22:30 +0200

    io

--- IO_test.cpp
+++ IO_test.cpp
@@ -1,25 +1,54 @@
+#include <fstream>
 #include <iostream>
-#include <iomanip>
+#include <cstring>
 using namespace std;
+
+struct student{
+    int num;
+    char name[20];
+    float score;
+};
 
 int main()
 {
-    double a=123.456,b=3.14159,c=-3214.67;
+    student stud[5] {1001,"Li",85,1002,"Fun",97.5,1004,"Wang",54,1006,"Tan",76.5,1010,"ling",96};
 
-    cout.precision( 2 );
-    // cout.width( 10 );
-    cout.setf( ios::right );
-    cout.setf( ios::fixed );
-    // cout << setiosflags( ios::fixed ) << setiosflags( ios::right ) << setprecision( 2 );
-    cout << setw( 10 ) << a << '\n';
-    cout << setw( 10 ) << b << '\n';
-    cout << setw( 10 ) << c << endl;
+    fstream iofile( "stud.dat", ios_base::out | ios_base::binary );
+    if( !iofile )
+    {
+        cerr << "Open error!" << '\n';
+        abort();
+    }
 
-    char ch[20];
-    cin >> ch;
-    cout << ch;
-    cin.getline( ch, 20, '/' );
-    cout << ch << endl;
+    iofile.write( reinterpret_cast<char*>(&stud[0]), sizeof( stud ) );
+    iofile.close();
+
+    iofile.open("stud.dat", ios_base::out |ios_base::in | ios_base::binary );
+    student stud1[5];
+    for( int i = 0; i < 5; i += 2)
+    {
+        iofile.seekg( i*sizeof( stud[i] ), ios_base::beg );
+        iofile.read( reinterpret_cast<char*>(&stud1[i/2]), sizeof( stud1[0] ) );
+
+        cout << stud1[i/2].num << " " << stud1[i/2].name << " " << stud1[i/2].score << '\n';
+    }
+    cout << endl;
+
+    stud[2].num = 1012;
+    strcpy( stud[2].name, "Wu" );
+    stud[2].score = 60;
+
+    iofile.seekp( 2*sizeof( stud[0] ), ios_base::beg );
+    iofile.write( reinterpret_cast<char*>( &stud[2] ), sizeof( stud[2] ) );
+    iofile.seekp( 0, ios_base::beg );
+
+    for( int i = 0; i < 5; ++i )
+    {
+        iofile.read( reinterpret_cast<char*>( &stud[i] ), sizeof( stud[i] ) );
+        cout << stud[i].num << " " << stud[i].name << " " << stud[i].score << '\n';
+    }
+
+    iofile.close();
 
     return 0;
 }
