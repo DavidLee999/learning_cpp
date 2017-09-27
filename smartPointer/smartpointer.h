a5a748dfdb39e6431494220e2f0ce120b536b87b
DavidLee999 <MoonLee003@outlook.com>
2017-09-27 17:01:42 +0200

    Smart pointer

--- smartPointer/smartpointer.h
+++ smartPointer/smartpointer.h
@@ -1,15 +1,59 @@
+template <typename>
+class SmartPointer;
+
+template <typename T>
+class uPtr {
+    private:
+        uPtr(T* p) : ptr { p }, count { 1 } {}
+
+        ~uPtr()
+        {
+            delete ptr;
+        }
+
+        T* ptr;
+        int count;
+
+        friend class SmartPointer<T>;
+};
 template <typename T>
 class SmartPointer {
     public:
-        SmartPointer() : mPointer { nullptr } {}
-        SmartPointer(T* p) : mPointer { p } {}
+        SmartPointer(T* p) : mPointer { new uPtr<T>{ p } } {}
+
+        SmartPointer(const SmartPointer& rhs) : mPointer { rhs.mPointer }
+        {
+            ++mPointer->count;
+        }
+
+        SmartPointer& operator = (const SmartPointer& rhs)
+        {
+            ++rhs.mPointer->count;
+
+            if (--mPointer->count == 0)
+                delete mPointer;
+
+            mPointer = rhs.mPointer;
+
+            return *this;
+        }
 
         ~SmartPointer()
         {
-            if (mPointer)
+            if (--mPointer->count == 0)
                 delete mPointer;
         }
 
+        T& operator * () const
+        {
+            return *(mPointer->ptr);
+        }
+
+        T* operator -> () const
+        {
+            return mPointer->ptr;
+        }
+
     private:
-        T* mPointer;
+        uPtr<T>* mPointer;
 };
--- smartPointer/sptestcase1.cpp
+++ smartPointer/sptestcase1.cpp
@@ -11,20 +11,24 @@ class SomeClass {
         }
 };
 
-void testcase1()
-{
-    SmartPointer<char> spunknown;
-
-    SmartPointer<char> spnull = nullptr;
-
-    SmartPointer<SomeClass> spclass = new SomeClass;
-
-    SmartPointer<const char> spstr = "Hello world!";
-}
-
 int main()
 {
-    testcase1();
+    int* i = new int{ 2 };
+    {
+        SmartPointer<int> ptr1 { i };
+        {
+            SmartPointer<int> ptr2 { ptr1 };
+            {
+                SmartPointer<int> ptr3 = nullptr;
+
+                ptr3 = ptr2;
+
+                cout << *ptr1 << endl;
+                *ptr3 = 20;
+                cout << *ptr2 << endl;
+            }
+        }
+    }
 
     return 0;
 }
